7b6b481db9f71ce96f8dbf797b7ec5aabda64b75
maximiliano ibarra <maxi.8379@gmail.com>
2022-06-05 22:24:07 -0300

    entrega desafio1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AsideComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
